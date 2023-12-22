7c30fb637cc86bb5f98b972291c1c10bdc07196b
Alex Brook <43730469+alex-brook@users.noreply.github.com>
2023-12-22 14:09:30 +0000

    ray tracing: 5

--- ray-tracing/lib/main.rb
+++ ray-tracing/lib/main.rb
@@ -5,8 +5,28 @@ require_relative "ray"
 class Main
   using NumericVec3
 
+  def self.hit_sphere(center, radius, ray)
+    oc = ray.origin - center
+    a = Vec3.dot(ray.direction, ray.direction)
+    b = 2.0 * Vec3.dot(oc, ray.direction)
+    c = Vec3.dot(oc, oc) - radius * radius
+
+    discriminant = b * b - 4 * a * c
+
+    if discriminant < 0 
+      -1.0
+    else
+      (-b - Math.sqrt(discriminant)) / (2.0 * a)
+    end
+  end
+
   def self.ray_color(ray)
-    # Vec3.new(0, 0, 0).color
+    t = hit_sphere(Point3.new(0, 0, -1), 0.5, ray)
+    if t > 0.0
+      n = Vec3.unit_vector(ray.at(t) - Vec3.new(0, 0, -1))
+      return 0.5 * Color.new(n.x + 1, n.y + 1, n.z + 1)
+    end
+
     unit_direction = Vec3.unit_vector(ray.direction)
     a = 0.5 * (unit_direction.y + 1.0)
     (1.0 - a) * Color.new(1.0, 1.0, 1.0) + a * Color.new(0.5, 0.7, 1.0)
@@ -15,12 +35,12 @@ class Main
   def self.main
     aspect_ratio = 16.0 / 9.0
     image_width = 400
-    image_height = [image_width / aspect_ratio, 1].max.to_i
+    image_height = [image_width.fdiv(aspect_ratio), 1].max.to_i
 
     # camera
     focal_length = 1.0
     viewport_height = 2.0
-    viewport_width = viewport_height * (image_width / image_height)
+    viewport_width = viewport_height * (image_width.fdiv(image_height))
     camera_center = Point3.new(0, 0, 0)
 
     # vectors for moving around the image
